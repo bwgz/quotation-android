ac414d6c815c6a3ba37b695801e88fc22abf0e46
Bruce Green <bwgz57@gmail.com>
2013-03-23 15:32:56 -0500

    Code cleanup.

--- projects/android/qotd/gen/org/bwgz/android/developer/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package org.bwgz.android.developer;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
--- projects/android/qotd/gen/org/bwgz/qotd/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package org.bwgz.qotd;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
